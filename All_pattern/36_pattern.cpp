bb78334805e45a3393333b9881bfa7216dfa0179
Aham0803 <ahamgolash9340@gmail.com>
2026-02-13 08:10:36 +0530

    notes

--- All_pattern/36_pattern.cpp
+++ All_pattern/36_pattern.cpp
@@ -8,3 +8,79 @@
 
 //  n = 7
 
+#include <iostream>
+using namespace std;
+int main()
+{
+    int m;
+    cin >> m;
+    int n = (m + 1) / 2;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        if (i == 1)
+        {
+            cout << "1";
+        }
+        if (i != 1)
+        {
+            for (int j = 1; j <= 1; j++)
+            {
+                cout << i - 1;
+            }
+        }
+
+        for (int j = 1; j <= 2 * i - 2; j++)
+        {
+
+            cout << " ";
+        }
+        if (i != 1)
+        {
+            for (int j = 1; j <= 1; j++)
+            {
+                cout << i - 1;
+            }
+        }
+        cout << endl;
+    }
+    // lower
+
+    for (int i = n - 1; i >= 1; i--)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        if (i == 1)
+        {
+            cout << "1";
+        }
+        if (i != 1)
+        {
+            for (int j = 1; j <= 1; j++)
+            {
+                cout << i - 1;
+            }
+        }
+
+        for (int j = 1; j <= 2 * i - 2; j++)
+        {
+
+            cout << " ";
+        }
+        if (i != 1)
+        {
+            for (int j = 1; j <= 1; j++)
+            {
+                cout << i - 1;
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
