633469337e09052d9d4dee9d9ea9ad53790ce2f6
Aham0803 <ahamgolash9340@gmail.com>
2026-03-22 01:18:24 +0530

    notes

--- LeetCode/75_sort_color.cpp
+++ LeetCode/75_sort_color.cpp
@@ -27,3 +27,24 @@ int main()
     cout << endl;
     return 0;
 }
+
+// by dutch national flag algo (most effective)
+
+//     int n = nums.size();
+//     int low = 0 , mid = 0 , high = n-1;
+//     while(mid <= high){
+//         if(nums[mid] == 0){
+//              swap(nums[low] , nums[mid]);
+//              mid++ ;
+//              low++;
+//         }
+//         else if(nums[mid] == 1){
+//             mid++;
+//         }
+//         else{
+//             swap(nums[high] , nums[mid]);
+//             high--;
+//         }
+//     }
+//     }
+// };
