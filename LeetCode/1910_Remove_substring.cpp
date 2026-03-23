634d55b0a49198a5677223d0d622c6dd5a5fef68
Aham0803 <ahamgolash9340@gmail.com>
2026-03-24 00:34:39 +0530

    notes

---
+++ LeetCode/1910_Remove_substring.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string s;
+    cin >> s;
+    string part;
+    cin >> part;
+
+    while(s.length() > 0 && s.find(part) < s.length()){
+        s.erase(s.find(part) , part.length());
+    }
+
+    cout << s;
+
+return 0;
+}
