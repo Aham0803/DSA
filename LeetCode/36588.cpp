68f379ec8504dccf2c9b76d4a311152547b08827
Aham0803 <ahamgolash9340@gmail.com>
2026-04-16 23:35:42 +0530

    notes

---
+++ LeetCode/36588.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int gcdOfOddEvenSums()
+{
+    int n;
+    cin >> n;
+    int OddSum = 0;
+    int EvenSum = 0;
+    int ans = 1;
+    for (int i = 1; i <= 2 * n; i++)
+    {
+        if (i % 2 != 0)
+        {
+            OddSum += i;
+        }
+        else
+        {
+            EvenSum += i;
+        }
+    }
+    for (int i = 0; i <= min(OddSum, EvenSum); i++)
+    {
+        if (OddSum % i == 0 and EvenSum % i == 0)
+        {
+            ans = i;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
+
+int main()
+{
+    gcdOfOddEvenSums();
+    return 0;
+}
